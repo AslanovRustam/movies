bd12caa1a7a95fd15c6bba32d3ae061db837a67e
Aslanov Rustam <aslanovrg@gmail.com>
2024-07-22 15:47:21 +0300

    upd baseurl for axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 const API_KEY = "65125fece056379d46954e3213fdb44a";
-const BASE_URL = "https://api.themoviedb.org/3/";
+const BASE_URL = "https://api.themoviedb.org/3";
 
 const moviesTrendingFetch = async () => {
   try {
     const response = await axios.get(
-      `${BASE_URL}trending/movie/day?api_key=${API_KEY}`
+      `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`
     );
     return response.data.results;
   } catch (error) {
@@ -16,7 +16,7 @@ const moviesTrendingFetch = async () => {
 const moviesSearchFetch = async (query) => {
   try {
     const response = await axios.get(
-      `${BASE_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
+      `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
     );
     return response.data.results;
   } catch (error) {
